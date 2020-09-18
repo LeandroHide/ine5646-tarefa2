87a371997607e6f6a16172d9c1e2b5cd60edeb82
Leandro Hideki <51826138+LeandroHide@users.noreply.github.com>
2020-09-18 18:05:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
 class Listicle {
-    constructor(name, subtitle, summary, image) {
-        this.name = name;
-        this.subtitle = subtitle;
-        this.summary = summary;
-        this.image = image;
-        this.nome = document.createElement("H1");
-        this.nome.innerText = name;
-        this.nome.id = "item_nome";
-        this.subtitulo = document.createElement("H2");
-        this.subtitulo.innerText = subtitle;
-        this.subtitulo.id = "item_subtitulo";
-        this.sinopse = document.createElement("P");
-        this.sinopse.innerText = summary;
-        this.sinopse.id = "item_sinopse";
-        this.figura = document.createElement("IMG");
-        this.figura.src = image;
-        this.figura.id = "item_imagem";
-        this.listaTopicos = document.getElementById("listaMangas");
-        this.listaItems = document.createElement("DIV")
-        this.listaItems.id = "item_lista";
-        this.listaItems.appendChild(this.nome)
-        this.listaItems.appendChild(this.subtitulo)
-        this.listaItems.appendChild(this.sinopse)
-        this.listaItems.appendChild(this.figura)
-        this.listaTopicos.appendChild(this.listaItems)
-    }
+  constructor(name, subtitle, summary, image) {
+      this.nome = this.create("H1", name, "item_nome");
+      this.subtitulo = this.create("H2", subtitle, "item_subtitulo");
+      this.sinopse = this.create("P", summary, "item_sinopse");
+      this.figura = this.createImage("IMG", image, "item_imagem");
+
+      this.listaTopicos = this.juntaTudo(this.nome, this.subtitulo, this.sinopse, this.figura);
+
+  }
+
+  create(type, content, id) {
+      const elemento = document.createElement(type);
+      elemento.innerText = content;
+      elemento.id = id;
+      return elemento;
+  }
+
+  createImage(type, content, id) {
+      const elemento = document.createElement(type);
+      elemento.src = content;
+      elemento.id = id;
+      return elemento;
+  }
+
+
+  juntaTudo(nome, subtitulo, sinopse, figura, heart) {
+      let listaTopicos = document.getElementById("listaMangas");
+
+      let listaItems = document.createElement("DIV");
+      listaItems.id = "item_lista";
+
+
+      listaItems.appendChild(nome);
+      listaItems.appendChild(subtitulo);
+      listaItems.appendChild(sinopse);
+      listaItems.appendChild(figura);
+
+
+      listaTopicos.appendChild(listaItems);
+
+      return listaTopicos;
+  }
+
+
 }
 
 topico1 = new Listicle("The New Gate", "Kazanami, Shinogi & Miwa, Yoshiyuki (Arte)",
@@ -38,4 +56,4 @@ topico2 = new Listicle("One Punch-Man", "ONE & Murata, Yusuke (Arte)",
 
 topico3 = new Listicle("Shokugeki no Souma", "Jang Sung-Lak",
                        "O sonho de Souma Yukihira é se tornar um chef de primeira no restaurante de seu pai e superar suas habilidades de culinária, mas assim que se gradua no ensino fundamental, seu pai, Jouichirou Yukihira, fecha o restaurante para cozinhar nos Estados Unidos. Embora oprimido, o espírito de luta de Souma se reacende pelo desafio de Jouichirou no qual é sobreviver numa escola de culinária de elite onde apenas 10% dos estudantes se graduam. Poderá Souma sobreviver?",
-                       "imagens/souma.jpg")
+                       "imagens/souma.png")
